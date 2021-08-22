ccc622661fc4ee8af4893d46cd6f1976adeaf5f8
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2021-08-23 06:09:42 +0900

    user 정보를 받아와 전달하는 법

--- security/src/main/java/springboot/security/controller/IndexController.java
+++ security/src/main/java/springboot/security/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -57,7 +58,9 @@ public class IndexController{
     }
 
     @GetMapping("/myPage")
-    public String myPage(){
+    public String myPage(Model model, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        User user = principalDetails.getUser();
+        model.addAttribute("user", user);
         return "../static/src/html/myPage";
     }
 
